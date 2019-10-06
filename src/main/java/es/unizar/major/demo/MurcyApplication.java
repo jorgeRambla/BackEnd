40f55af1a689fcb900874bac9722987b1a56c1cb
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-10-06 18:49:41 +0200

    Project init

---
+++ src/main/java/es/unizar/major/demo/MurcyApplication.java
@@ -0,0 +1,13 @@
+package es.unizar.major.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurcyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurcyApplication.class, args);
+	}
+
+}
