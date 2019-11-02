0b137bada145131d0402228531e4af05cd2c2ade
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-11-02 21:36:44 +0100

    Revert "develop: cors second try"
    This reverts commit ad0f1db08424e01876614863d8fa81b72f57d553.

--- src/main/java/es/unizar/murcy/configuration/WebSecurityConfig.java
+++ src/main/java/es/unizar/murcy/configuration/WebSecurityConfig.java
@@ -15,14 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 
 @Configuration
@@ -64,28 +56,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.unmodifiableList(new ArrayList<>(Arrays.asList("*"))));
-        configuration.setAllowedMethods(Collections.unmodifiableList(new ArrayList<>(Arrays.asList("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"))));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Collections.unmodifiableList(new ArrayList<>(Arrays.asList("Authorization", "Cache-Control", "Content-Type"))));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity.cors();
 
         httpSecurity//.csrf().disable()
 
