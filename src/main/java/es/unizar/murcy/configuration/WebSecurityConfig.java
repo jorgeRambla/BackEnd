6aa7634b4ccf673ad1c42f857bdbb67a8b3dbdcd
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-11-05 22:21:00 +0100

    develop: cors again

--- src/main/java/es/unizar/murcy/configuration/WebSecurityConfig.java
+++ src/main/java/es/unizar/murcy/configuration/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 
 @Configuration
@@ -59,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.cors().and()
+        httpSecurity.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
 
                 .authorizeRequests().antMatchers("/api/user/login","/api/user", "/api/user/confirm/**").permitAll()
                 .antMatchers("/h2-console/**/**").permitAll()
