fbd3a93897fdd268ceef45bddbaa7850b77c4732
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-11-06 19:25:23 +0100

    develop: comienzo test

---
+++ src/test/java/es/unizar/murcy/repository/TokenRepositoryTest.java
@@ -0,0 +1,12 @@
+package es.unizar.murcy.repository;
+
+import es.unizar.murcy.MurcyApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MurcyApplication.class)
+public class TokenRepositoryTest {
+
+}
