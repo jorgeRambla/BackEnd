98f6f29423bc989fd2cac53c13140d1714d9086b
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-11-25 18:00:07 +0100

    Jorge: code clean

--- src/main/java/es/unizar/murcy/model/extendable/jpa/AuditableEntity.java
+++ src/main/java/es/unizar/murcy/model/extendable/jpa/AuditableEntity.java
@@ -7,8 +7,12 @@ import java.util.Date;
 
 @MappedSuperclass
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
-public class AuditableEntity extends RemovableEntity{
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AuditableEntity{
+
+    private static final int MAX_DELETION_DAYS = 4;
+    private static final long MAX_DELETION_MILLIS = MAX_DELETION_DAYS * 24L * 3600L * 1000L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter
@@ -24,10 +28,29 @@ public class AuditableEntity extends RemovableEntity{
     @Setter
     private Date modifiedDate;
 
+    @Getter
+    @Setter
+    private boolean deleted;
+
+    @Getter
+    @Setter
+    private Date deletionDate;
+
+    @Getter
+    @Setter
+    private Date forceDeleteDate;
+
     public AuditableEntity() {
         super();
         Date date = new Date();
         this.createDate = date;
         this.modifiedDate = date;
+        this.deleted = false;
+    }
+
+    public void delete() {
+        this.deleted = true;
+        this.deletionDate = new Date();
+        this.forceDeleteDate = new Date(System.currentTimeMillis() + MAX_DELETION_MILLIS);
     }
 }
