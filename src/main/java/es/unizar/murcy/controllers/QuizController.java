1f248a5b6ad0f4fdaed4f4c57a038c7b26a02ef6
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-11-27 19:25:40 +0100

    Jorge: quiz controller started

--- src/main/java/es/unizar/murcy/controllers/QuizController.java
+++ src/main/java/es/unizar/murcy/controllers/QuizController.java
@@ -1,10 +1,48 @@
 package es.unizar.murcy.controllers;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+/*@CrossOrigin
 @RestController
 public class QuizController {
+
+    @CrossOrigin
+    @PostMapping(value = "/api/quiz")
+    public ResponseEntity create() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/api/quiz/list")
+    public ResponseEntity fetchCurrentUserQuizList() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/api/quiz/list/{id}")
+    public ResponseEntity fetchUserByIdQuizList() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/api/quiz/list/{id}")
+    public ResponseEntity fetchQuizById() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @CrossOrigin
+    @PutMapping(value = "/api/quiz/list/{id}")
+    public ResponseEntity update() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @CrossOrigin
+    @DeleteMapping(value = "/api/quiz/list/{id}")
+    public ResponseEntity delete() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
 }
+*/
