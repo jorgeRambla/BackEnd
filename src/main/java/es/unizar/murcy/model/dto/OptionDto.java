5fb349f7ac16d317bf8a19131d3ac361fd901dc4
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2020-01-13 13:23:17 +0100

    Jorge: swagger doc actualization

--- src/main/java/es/unizar/murcy/model/dto/OptionDto.java
+++ src/main/java/es/unizar/murcy/model/dto/OptionDto.java
@@ -6,14 +6,21 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OptionDto {
+
+    @Getter
+    @Setter
+    private long id;
+
     @Getter
     @Setter
     private String title;
+
     @Getter
     @Setter
     private boolean correct;
 
     public OptionDto(Option option) {
+        this.id = option.getId();
         this.title = option.getTitle();
         this.correct = option.getCorrect();
     }
