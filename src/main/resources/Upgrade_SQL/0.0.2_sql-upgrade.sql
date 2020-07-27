51363803adfe13348c8a74ceafb0ea2f38aa3c43
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2020-07-27 14:47:53 +0200

    Fix SQL error  # [skip ci]

--- src/main/resources/Upgrade_SQL/0.0.2_sql-upgrade.sql
+++ src/main/resources/Upgrade_SQL/0.0.2_sql-upgrade.sql
@@ -2,8 +2,7 @@
 
 alter table murcy_question alter column title type varchar(512) using title::varchar(512);
 alter table murcy_quiz alter column title type varchar(512) using title::varchar(512);
-alter table murcy_individual_answer	add options varchar(5096);
 UPDATE murcy_individual_answer
-   SET options=(SELECT STRING_AGG(options_id, ',') FROM murcy_individual_answer_options WHERE murcy_individual_answer.id=murcy_individual_answer_options.individual_answer_id)
+   SET options=(SELECT string_agg(options_id::text, ',') FROM murcy_individual_answer_options WHERE murcy_individual_answer.id=murcy_individual_answer_options.individual_answer_id)
     where id != -1;
 drop table murcy_individual_answer_options;
